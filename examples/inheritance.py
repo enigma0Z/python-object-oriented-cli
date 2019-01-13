2f0b1f6b90f88750f00516749aab4b8192e2d376
Johnathan Bell <enigma.0ZA@gmail.com>
2019-01-13 17:39:12 -0500

    Add examples

---
+++ examples/inheritance.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Example using inheritance
+"""
+
+import sys
+import re
+
+sys.path.append("../")
+from oocli import interpreter, base, entrypoint
+
+class Re(base.Command):
+    """
+    Given a regex, matches it (re.match()) against the input text
+    """
+
+    def __init__(self):
+        super().__init__(
+            name="re",
+            description="Match regexes")
+
+    def initParser(self):
+        super().initParser()
+        self._parser.add_argument("regex", help="The regex")
+        self._parser.add_argument("string", help="The thing to compare against")
+
+    def do(self, *args):
+        opts = self.parse(*args)
+        return re.match(opts.regex, opts.string)
+
+class Cmp(base.Command):
+    """
+    Given a string, compares it against the input text
+    """
+
+    def __init__(self):
+        super().__init__(
+            name="cmp",
+            description="Match string literal")
+
+    def initParser(self):
+        super().initParser()
+        self._parser.add_argument("compare", help="The string to compare")
+        self._parser.add_argument("string", help="The thing to compare against")
+
+    def do(self, *args):
+        opts = self.parse(*args)
+        return opts.compare == opts.string
+
+class Entrypoint(entrypoint.Command):
+    """
+    Entrypoint for Re
+    """
+
+    def __init__(self):
+        super().__init__(
+            description="OO CLI Example",
+            command=interpreter.Command(
+                description="Run comparisons (re or cmp)",
+                commands=[
+                    Re(),
+                    Cmp()
+                ]
+            )
+        )
+
+Entrypoint().do()
---
+++ examples/simple.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Quick & simple example of the built in command classes:
+
+    $ ./simple.py --help
+    <help text>
+
+    $ ./simple.py --list # Prints the output from the interpreter --list option
+    echo: Print the input to the terminal
+    outer: Interpret subcommands
+    - echo: Print the input to the terminal
+    - inner: Interpret subcommands
+      - echo: Print the input to the terminal
+
+    $ ./simple.py echo "test"
+    test
+    
+"""
+from oocli import interpreter, echo, entrypoint
+
+entrypoint.Command(
+    "oocli example",
+    interpreter.Command(
+        name=None,
+        commands=[
+            echo.Command(),
+            interpreter.Command(
+                name="Outer",
+                commands=[
+                    echo.Command(),
+                    interpreter.Command(
+                        name="Inner",
+                        commands=[
+                            echo.Command()
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+).do()
+
